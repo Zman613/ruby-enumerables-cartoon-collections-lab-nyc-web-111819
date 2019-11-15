62f52c1bb1a1673b4ae352ddb825ca01ac2c3116
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-11-15 02:20:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,8 @@ def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each_with_index do |cheese, i|
-    if strings.include?(cheese)
-      cheese
+    if strings.include?(cheese[i])
+      cheese[i]
     else
       nil
     end
